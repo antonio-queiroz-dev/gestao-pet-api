9e86e70d5c366a7e06f71e1763f2af0b34ff5dbc
antonio-scripts <antonioqueiroz.dev@gmail.com>
2026-01-30 23:40:50 -0300

    feat: adiciona validações no DTO de Tutor e nova exceção IllegalArgumentExceptio

--- src/main/java/com/example/desafioCadastro/dto/TutorCreateDto.java
+++ src/main/java/com/example/desafioCadastro/dto/TutorCreateDto.java
@@ -1,8 +1,19 @@
 package com.example.desafioCadastro.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
-public record TutorCreateDto(String nome,
-                             String email,
-                             String telefone) {
+public record TutorCreateDto(
+        @NotBlank(message = "O nome não pode estar em branco")
+        String nome,
+
+        @NotBlank(message = "O email não pode estar em branco")
+        @Email(message = "Formato de email inválido")
+        String email,
+
+        @NotBlank(message = "O telefone não pode estar em branco")
+        @Pattern(regexp = "\\d{10,11}",message = "O telefone deve ter de 10 a 11 dígitos")
+        String telefone) {
 }
--- src/main/java/com/example/desafioCadastro/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/desafioCadastro/exceptions/GlobalExceptionHandler.java
@@ -21,4 +21,16 @@ public class GlobalExceptionHandler {
         body.put("message", ex.getMessage());
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", "Error de validação");
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
 }
